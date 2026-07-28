980d9d3fd247e21a3de2110a38f4c15f1ca5c808
Busetty Balasubramanyam <balasubramanyam200517@gmail.com>
2026-07-28 19:25:21 +0530

    Time: 1 ms (98.08%) | Memory: 47 MB (56.74%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -2,16 +2,25 @@ class Solution {
 
     public int[][] insert(int[][] intervals, int[] newInterval) {
 
-        List<int[]> ans = new ArrayList<>();
-
-        int i = 0;
         int n = intervals.length;
 
+        // Maximum possible intervals = n + 1
+        int[][] ans = new int[n + 1][2];
+
+        int i = 0;
+        int k = 0;
+
+        // Step 1: Copy all intervals before newInterval
         while (i < n && intervals[i][1] < newInterval[0]) {
-            ans.add(intervals[i]);
+
+            ans[k][0] = intervals[i][0];
+            ans[k][1] = intervals[i][1];
+
+            k++;
             i++;
         }
 
+        // Step 2: Merge overlapping intervals
         while (i < n && intervals[i][0] <= newInterval[1]) {
 
             newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
@@ -20,13 +29,30 @@ class Solution {
             i++;
         }
 
-        ans.add(newInterval);
+        // Store merged interval
+        ans[k][0] = newInterval[0];
+        ans[k][1] = newInterval[1];
 
+        k++;
+
+        // Step 3: Copy remaining intervals
         while (i < n) {
-            ans.add(intervals[i]);
+
+            ans[k][0] = intervals[i][0];
+            ans[k][1] = intervals[i][1];
+
+            k++;
             i++;
         }
 
-        return ans.toArray(new int[ans.size()][]);
+        // Create exact size result
+        int[][] result = new int[k][2];
+
+        for (int j = 0; j < k; j++) {
+            result[j][0] = ans[j][0];
+            result[j][1] = ans[j][1];
+        }
+
+        return result;
     }
 }
